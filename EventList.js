afa9796b21ec2e2aedc9e1b65d5a488bc171d13f
willianwelbert <willianwelbert@yahoo.com.br>
2020-06-01 11:35:57 -0300

    end of lesson 1 - components

--- EventList.js
+++ EventList.js
@@ -1,18 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
+import EventCard from './EventCard';
+
+const renderEventCard = ({item}) => <EventCard event={item} />;
 
 const EventList = () => {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    const events = require('./db.json').events;
+    const intervalID = setInterval(() => {
+      setEvents(event => event.map( evt => ({
+        ...evt,
+        timer: Date.now(),
+      })))
+    }, 1000);
+
+    const events = require('./db.json').events.map((e) => ({
+      ...e,
+      date: new Date(e.date),
+    }));
     setEvents(events);
-  } , [])
+
+    return () => clearInterval(intervalID);
+  } , []);
+
+  const styles = StyleSheet.create({
+    list: {
+      flex: 1,
+      paddingTop: 20,
+      backgroundColor: '#F3F3F3'
+    },
+  });
 
   return (
     <FlatList
       data={events}
-      renderItem={({item}) => <Text>{item.title}</Text>}
+      style={styles.list}
+      renderItem={renderEventCard}
       keyExtractor={(item) => item.id}
     />
   );
